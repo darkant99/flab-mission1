9a07eea997506e655b20f5af670f5ccf87ee3fdb
JaewonKim <darkant9714@gmail.com>
2022-10-07 23:36:39 +0900

    init

---
+++ src/main/java/com/flab/mission1/FlabMission1Application.java
@@ -0,0 +1,13 @@
+package com.flab.mission1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlabMission1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlabMission1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/mission1/flabmission1/FlabMission1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.mission1.flabmission1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlabMission1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
