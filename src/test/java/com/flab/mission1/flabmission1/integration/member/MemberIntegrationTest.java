8e36422cd6894018fdf5bab090d5f649bef40a18
JaewonKim <darkant9714@gmail.com>
2022-10-08 14:46:02 +0900

    test: profile

--- src/test/java/com/flab/mission1/flabmission1/integration/member/MemberIntegrationTest.java
+++ src/test/java/com/flab/mission1/flabmission1/integration/member/MemberIntegrationTest.java
@@ -14,7 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SuppressWarnings("NonAsciiCharacters")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class MemberIntegrationTest {
